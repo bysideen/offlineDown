e274c6803b1f9d0b880fde58ac214a602c33cf83
GuNanHai <42151384+GuNanHai@users.noreply.github.com>
2020-08-22 01:18:39 +0800

    Delete v2rayC_tproxy.sh

--- v2rayScript/tproxy/v2rayC_tproxy.sh
+++
@@ -1,7 +0,0 @@
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-sysctl -p
-
-wget -O config.json https://raw.githubusercontent.com/GuNanHai/offlineDown/master/v2rayScript/config.json
-wget -O v2ray.service https://raw.githubusercontent.com/GuNanHai/offlineDown/master/v2rayScript/v2ray.service
-wget -O v2ray-linux-64.zip https://github.com/GuNanHai/offlineDown/raw/master/v2rayScript/v2ray-linux-64.zip
