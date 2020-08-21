605bd287769976b1a84ef04aa03c74b88ddef323
GuNanHai <42151384+GuNanHai@users.noreply.github.com>
2020-08-22 00:04:27 +0800

    Update v2ray-C-tproxy.sh

--- v2rayScript/tproxy/v2ray-C-tproxy.sh
+++ v2rayScript/tproxy/v2ray-C-tproxy.sh
@@ -2,10 +2,10 @@ echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
 
-wget -O config.json https://gitee.com/bysideen/offlineDown/raw/master/v2rayScript/tproxy/configC-tproxy.json
-wget -O v2ray.service https://gitee.com/bysideen/offlineDown/raw/master/v2rayScript/v2ray.service
-wget -O v2ray-linux-64.zip https://gitee.com/bysideen/offlineDown/raw/master/v2rayScript/v2ray-linux-64.zip
-wget -O tproxyrule.service https://gitee.com/bysideen/offlineDown/raw/master/v2rayScript/tproxy/tproxyrule.service
+wget -O config.json https://raw.githubusercontent.com/GuNanHai/offlineDown/master/v2rayScript/tproxy/configC-tproxy.json
+wget -O v2ray.service https://raw.githubusercontent.com/GuNanHai/offlineDown/master/v2rayScript/v2ray.service
+wget -O v2ray-linux-64.zip https://github.com/GuNanHai/offlineDown/raw/master/v2rayScript/v2ray-linux-64.zip
+wget -O tproxyrule.service https://raw.githubusercontent.com/GuNanHai/offlineDown/master/v2rayScript/tproxy/tproxyrule.service
 apt-get update
 apt-get install unzip
 
