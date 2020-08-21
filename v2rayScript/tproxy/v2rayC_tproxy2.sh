ec0f3cd5222fe9cce51cbb141a78c3d2cc0a8491
GuNanHai <42151384+GuNanHai@users.noreply.github.com>
2020-08-22 01:34:27 +0800

    Update and rename v2rayC_tproxy1.sh to v2rayC_tproxy2.sh

--- v2rayScript/tproxy/v2rayC_tproxy2.sh
+++ v2rayScript/tproxy/v2rayC_tproxy2.sh
@@ -6,3 +6,37 @@ wget -O config.json https://raw.githubusercontent.com/GuNanHai/offlineDown/maste
 wget -O v2ray.service https://raw.githubusercontent.com/GuNanHai/offlineDown/master/v2rayScript/v2ray.service
 wget -O v2ray-linux-64.zip https://github.com/GuNanHai/offlineDown/raw/master/v2rayScript/v2ray-linux-64.zip
 wget -O tproxyrule.service https://raw.githubusercontent.com/GuNanHai/offlineDown/master/v2rayScript/tproxy/tproxyrule.service
+
+apt-get update
+apt-get install unzip
+
+unzip v2ray-linux-64.zip
+rm v2ray-linux-64.zip
+mkdir /usr/bin/v2ray
+chmod +x v2ray v2ctl
+mv v2ray /usr/bin/v2ray/v2ray
+mv v2ctl /usr/bin/v2ray/v2ctl
+mkdir /etc/v2ray
+mkdir /var/log/v2ray
+mv config.json /etc/v2ray/
+mv geoip.dat /usr/bin/v2ray/geoip.dat
+mv geosite.dat  /usr/bin/v2ray/geosite.dat
+mv v2ray.service /etc/systemd/system/v2ray.service
+systemctl enable v2ray.service
+systemctl start v2ray.service
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
