cbd05b13f3416c0ff841ea33b6e4f68395605609
bysideen <42151384+bysideen@users.noreply.github.com>
2024-12-01 14:15:42 +0800

    Update offDown-V2ray.sh

--- offDown-V2ray.sh
+++ offDown-V2ray.sh
@@ -5,8 +5,11 @@ sysctl -p
 
 wget -O web.zip https://raw.githubusercontent.com/GuNanHai/offlineDown/master/web.zip
 wget -O aria2c.zip https://raw.githubusercontent.com/GuNanHai/offlineDown/master/aria2c.zip
-wget -O caddy.zip https://raw.githubusercontent.com/GuNanHai/offlineDown/master/caddy.zip
 wget -O aria2.service https://raw.githubusercontent.com/GuNanHai/offlineDown/master/aria2.service
+
+wget -O caddy.zip https://raw.githubusercontent.com/GuNanHai/offlineDown/master/caddy
+wget -O caddy.zip https://raw.githubusercontent.com/GuNanHai/offlineDown/master/Caddyfile
+
 
 wget -O config.json https://raw.githubusercontent.com/GuNanHai/offlineDown/master/v2rayScript/config.json
 
@@ -59,10 +62,10 @@ unzip aria2c.zip
 mv .aria2 ~/
 rm aria2c.zip
 
-unzip caddy.zip
+
 mv caddy /usr/local/bin/
 mv Caddyfile /usr/local/bin/
-rm caddy.zip
+
 
 mv aria2.service /etc/systemd/system/aria2.service
 systemctl enable aria2.service && systemctl start aria2.service
